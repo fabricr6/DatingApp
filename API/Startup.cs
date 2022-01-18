573d7c13b9604698db710fa72e7d69c8bc365710
Fabricio-Lobo <fabriciolobo62@gmail.com>
2022-01-18 14:26:12 -0600

    End of Section 4

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,8 +1,13 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using API.Data;
+using API.Extensions;
+using API.Interfaces;
+using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -12,6 +17,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 
 namespace API
@@ -19,24 +25,42 @@ namespace API
     public class Startup
     {
         private readonly IConfiguration _config;
-       
+
         public Startup(IConfiguration config)
         {
             _config = config;
-            
+
         }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddApplicationServices(_config);
+            services.AddControllers();
+            services.AddIdentityServices(_config);
+            // services.AddScoped<ITokenService, TokenService>();
+/*             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["TokenKey"])),
+                    ValidateAudience = false,
+                    ValidateIssuer = false
+                };
+
+            }); */
 
             var d = "https://localhost:4200";
             var allowedDomains = Array.ConvertAll(d.Split(','), p => p.Trim());
-            services.AddControllers();
+          
 
-            services.AddDbContext<DataContext>(options =>
+/*             services.AddDbContext<DataContext>(options =>
                 options.UseSqlServer(
-                    _config.GetConnectionString("DefaultConnection")));
+                    _config.GetConnectionString("DefaultConnection"))); */
 
             services.AddCors(options =>
             {
@@ -55,7 +79,7 @@ namespace API
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
             });
         }
-        
+
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
@@ -71,6 +95,8 @@ namespace API
             app.UseRouting();
 
             app.UseCors("AllowAny");
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
